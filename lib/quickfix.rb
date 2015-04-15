062c857ec179faa6f9bc7183f617f8c779bda096
Mike Gatny <mgatny@gmail.com>
2015-04-15 09:55:55 -0400

    Upgrade to quickfixj-1.6.0, includes FIX50sp{1,2}.

--- lib/quickfix.rb
+++ lib/quickfix.rb
@@ -1,9 +1,14 @@
 require 'java'
 
-require 'quickfixj-all-1.5.3.jar'
-require 'mina-core-1.1.7.jar'
-require 'slf4j-api-1.6.3.jar'
-require 'slf4j-jdk14-1.6.3.jar'
+require 'quickfixj-all-1.6.0.jar'
+require 'dom4j-1.6.1.jar'
+require 'jaxen-1.1.4.jar'
+require 'jcl-over-slf4j-1.7.12.jar'
+require 'je-2.1.30.jar'
+require 'mina-core-2.0.9.jar'
+require 'proxool-0.9.1.jar'
+require 'slf4j-api-1.7.12.jar'
+require 'slf4j-jdk14-1.7.12.jar'
 
 def quickfix
   Java::quickfix
